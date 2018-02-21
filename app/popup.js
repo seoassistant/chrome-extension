b255b286bad6c3a1d001603e8a5b64b4bc3ce7d0
Jota Teles <josetelesmaciel@gmail.com>
2018-02-20 22:20:43 -0300

    Exibe o titulo da pagina atual

--- app/popup.js
+++ app/popup.js
@@ -4,19 +4,34 @@ function showLinks() {
   document.getElementById("links").innerHTML = "<h1>"+ allLinks.length +"</h1>";
 }
 
-chrome.extension.onRequest.addListener(function(links) {
-  allLinks = links;
-  showLinks();
+chrome.runtime.onMessage.addListener(function(request) {
+    if (request.action === "getSource") {
+        message.innerText = request.source.match(/<title.*>(.*)<\/title>/g);
+    }
 });
 
-window.onload = function() {
-    chrome.windows.getCurrent(function getCurrent(currentWindow) {
-        chrome.tabs.query({active: true, windowId: currentWindow.id},
-            function(activeTabs) {
-                chrome.tabs.executeScript(
-                    activeTabs[0].id, {file: 'send_links.js', allFrames: true}
-                );
-            });
+function onLoadWindow() {
+
+    var message = document.querySelector('#message');
+
+    chrome.tabs.executeScript(null, {
+        file: "send_links.js"
+    }, function() {
+        // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+        if (chrome.runtime.lastError) {
+            message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
         }
-    );
-};
+    });
+
+    // chrome.windows.getCurrent(function getCurrent(currentWindow) {
+    //     chrome.tabs.query({active: true, windowId: currentWindow.id},
+    //         function(activeTabs) {
+    //             chrome.tabs.executeScript(
+    //                 activeTabs[0].id, {file: 'send_links.js', allFrames: true}
+    //             );
+    //         });
+    //     }
+    // );
+}
+
+window.onload = onLoadWindow;
